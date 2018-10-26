82bd612ae27815f4368795ac22d6ad0a9bc28c7c
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-26 18:24:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,5 +2,16 @@
 require 'pry'
 class Anagram
 
+attr_accessor :word
 
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+     result = @word.split("").sort
+     array.select do |element|
+       result == element.split("").sort
+     end
+   end
 end
